73cdc614617ab8bc82ac814d0554cba0a04b38c6
TechCode18 <biswanath.1193@gmail.com>
2018-05-13 12:30:06 +0530

    This is my 1st update

---
+++ Test.java
@@ -0,0 +1,10 @@
+class Test 
+{
+	public static void main(String[] args) 
+	{
+        int i = 10;
+		int j = 20;
+		System.out.println("Hello World!"+i+j);
+		System.out.pringln("Ststus chaged");
+	}
+}
